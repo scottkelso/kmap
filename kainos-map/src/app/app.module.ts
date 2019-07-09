152957d221c5edc3b87b22b314fee503c8c0769c
Matthew Fox <matthew.fox@kainos.com>
2019-07-09 12:43:02 +0100

    adding git ignore

--- kainos-map/src/app/app.module.ts
+++ kainos-map/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcourseComponent
   ],
   imports: [
     BrowserModule
